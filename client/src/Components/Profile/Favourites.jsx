b9018aed8a62d6bf95d6c068523c3681faf4de75
Pratham Sawant <codevps07@gmail.com>
2024-08-23 23:17:51 +0530

    email removal

--- client/src/Components/Profile/Favourites.jsx
+++ client/src/Components/Profile/Favourites.jsx
@@ -1,61 +1,73 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import BookCard from '../BookCard';
-import { Hourglass } from "react-loader-spinner"
-import star from "../../assets/star.png"
-import toast from 'react-hot-toast';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import BookCard from "../BookCard";
+import { Hourglass } from "react-loader-spinner";
+import star from "../../assets/star.png";
+import toast from "react-hot-toast";
 
 function Favourites() {
-    const [favouriteBooks, setfavouriteBooks] = useState();
-    const [loader, setloader] = useState(true);
-    const headers = {
-        authorization: `Bearer ${localStorage.getItem('token')}`,
-        id: localStorage.getItem('id')
-    }
+  const [favouriteBooks, setfavouriteBooks] = useState();
+  const [loader, setloader] = useState(true);
+  const headers = {
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+    id: localStorage.getItem("id"),
+  };
 
-    useEffect(() => {
-        const fetch = async () => {
-            try {
-                const response = await axios.get(`/api/v1/get-all-fav-books`, { headers });
-                setfavouriteBooks(response.data.favouriteBooks);
-                setloader(false);
-            } catch (error) {
-                toast.error(error.response.data.message)
-            }
-        }
-        fetch();
-    }, [favouriteBooks]);
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await axios.get(`/api/v1/get-all-fav-books`, {
+          headers,
+        });
+        setfavouriteBooks(response.data.favouriteBooks);
+        setloader(false);
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    };
+    fetch();
+  }, [favouriteBooks]);
 
-
-    return (
-        <div className=''>
-            {
-                loader && <div className=' w-full flex justify-center items-center p-10 h-[85vh] '>
-                    <Hourglass
-                        visible={loader}
-                        height="90"
-                        width="90"
-                        ariaLabel="hourglass-loading"
-                        wrapperStyle={{}}
-                        wrapperClass=""
-                        colors={['#306cce', '#72a1ed']}
-                    />
-                </div>
-            }
-            {
-                favouriteBooks && favouriteBooks.length == 0 && <div className=' flex flex-col justify-center items-center h-auto md:h-[85vh] gap-5 '>
-                    <h1 className=' text-3xl md:text-5xl text-zinc-500 font-semibold'>No favourite books</h1>
-                    <img src={star} alt="star" className=' h-20 md:h-24' />
-                </div>
-            }
-            {favouriteBooks && favouriteBooks.length > 0 && <> <h1 className=' text-3xl text-zinc-500 font-semibold'>Your Favourite Books</h1>
-                <div className=' grid grid-cols-1 md:grid-cols-2 lg:grids-cols-3 lg:grid-cols-3 gap-6 mt-10'>
-                    {favouriteBooks.map((items, i) => (
-                        <div key={i}><BookCard data={items} favourite={true} /></div>
-                    ))}
-                </div></>}
+  return (
+    <div className="">
+      {loader && (
+        <div className=" w-full flex justify-center items-center p-10 h-[85vh] ">
+          <Hourglass
+            visible={loader}
+            height="90"
+            width="90"
+            ariaLabel="hourglass-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            colors={["#306cce", "#72a1ed"]}
+          />
         </div>
-    )
+      )}
+      {favouriteBooks && favouriteBooks.length == 0 && (
+        <div className=" flex flex-col justify-center items-center h-auto md:h-[85vh] gap-5 ">
+          <h1 className=" text-3xl md:text-5xl text-zinc-500 font-semibold">
+            No favourite books
+          </h1>
+          <img src={star} alt="star" className=" h-20 md:h-24" />
+        </div>
+      )}
+      {favouriteBooks && favouriteBooks.length > 0 && (
+        <>
+          {" "}
+          <h1 className=" text-3xl text-zinc-500 font-semibold">
+            Your Favourite Books
+          </h1>
+          <div className=" grid grid-cols-1 md:grid-cols-2 lg:grids-cols-3 lg:grid-cols-3 gap-6 mt-10">
+            {favouriteBooks.map((items, i) => (
+              <div key={i}>
+                <BookCard data={items} favourite={true} />
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default Favourites
+export default Favourites;
--- client/src/Components/Profile/UserOrderHistory.jsx
+++ client/src/Components/Profile/UserOrderHistory.jsx
@@ -1,86 +1,129 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Hourglass } from "react-loader-spinner";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import emptyOrder from "../../assets/empty-order.png";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 
 function UserOrderHistory() {
+  const [order, setorder] = useState();
+  const [loader, setloader] = useState(true);
 
-    const [order, setorder] = useState();
-    const [loader, setloader] = useState(true);
+  const headers = {
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+    id: localStorage.getItem("id"),
+  };
 
-    const headers = {
-        authorization: `Bearer ${localStorage.getItem("token")}`,
-        id: localStorage.getItem('id')
-    }
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await axios.get(`/api/v1/get-user-orders`, {
+          headers,
+        });
+        setorder(response.data.data);
+        setloader(false);
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    };
 
-    useEffect(() => {
-        const fetch = async () => {
-            try {
-                const response = await axios.get(`/api/v1/get-user-orders`, { headers })
-                setorder(response.data.data);
-                setloader(false);
-            } catch (error) {
-                toast.error(error.response.data.message)
-            }
-        }
+    fetch();
+  }, [order]);
 
-        fetch();
-    }, [order])
+  return (
+    <>
+      {loader && (
+        <div className="  w-full flex justify-center items-center p-10 h-[80vh]">
+          <Hourglass
+            visible={loader}
+            height="100"
+            width="100"
+            ariaLabel="hourglass-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            colors={["#306cce", "#72a1ed"]}
+          />
+        </div>
+      )}
 
-    return (
-        <>
+      {order && order.length == 0 && (
+        <div className="flex flex-col justify-center items-center h-auto md:h-[90vh] space-y-3">
+          <h1 className=" text-4xl md:text-5xl font-semibold text-zinc-400">
+            No Any Order
+          </h1>
+          <img src={emptyOrder} alt="Empty Order" className=" h-48" />
+        </div>
+      )}
 
-            {loader && <div className='  w-full flex justify-center items-center p-10 h-[80vh]'>
-                <Hourglass
-                    visible={loader}
-                    height="100"
-                    width="100"
-                    ariaLabel="hourglass-loading"
-                    wrapperStyle={{}}
-                    wrapperClass=""
-                    colors={['#306cce', '#72a1ed']}
-                />
-            </div>}
-
-            {
-                order && order.length == 0 && <div className='flex flex-col justify-center items-center h-auto md:h-[90vh] space-y-3'>
-                    <h1 className=' text-4xl md:text-5xl font-semibold text-zinc-400'>No Any Order</h1>
-                    <img src={emptyOrder} alt='Empty Order' className=' h-48' />
-                </div>
-            }
-
-            {order && order.length > 0 && <div className=' md:p-4 p-1 space-y-8'>
-                <h1 className=' text-4xl text-zinc-500 font-semibold'>Your Order History</h1>
-                <div className=' w-full bg-zinc-800'>
-                    <div className=' text-zinc-200 text-center border-[1px] border-zinc-700 flex text-sm md:text-xl  '>
-                        <div className=' p-2 border-[1px] flex justify-center items-center  border-zinc-600 w-[5%]'>Sr.</div>
-                        <div className=' p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[25%]'>Books</div>
-                        <div className=' p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[30%]'>Description</div>
-                        <div className=' p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[10%]'>Price</div>
-                        <div className=' p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[18%] '>Status</div>
-                        <div className=' p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[13%]'>Mode</div>
-                    </div>
-                    <div className=' text-sm w-full '>
-                        {order && order.map((items, i) => (
-                            <div key={i} className="  font-semibold text-zinc-300 text-center border-[1px] text-[12px] md:text-sm  h-fit border-zinc-600 w-full flex">
-                                <div className='p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[5%]'>{i + 1}</div>
-                                <div className='p-2 border-[1px] flex justify-center items-center border-zinc-600 hover:underline hover:text-blue-500 transition-all duration-300 w-[25%]'><Link to={`/view-book-details/${items.book._id}`}>{items.book.title}</Link></div>
-                                <div className='p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[30%]'>{items.book.desc.slice(0, 40)}...</div>
-                                <div className='p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[10%]'>&#8377; {items.book.price}</div>
-                                <div className='p-2 border-[1px] flex justify-center items-center border-zinc-600 text-green-500 w-[18%]' >{items.status === "Ordered Placed" ? (<p className=' text-yellow-500'>{items.status}</p>) : items.status === "Cancelled" ? (<p className='text-red-500'>{items.status}</p>) : items.status === "Delivered" ? (<p className=' text-blue-600'>{items.status}</p>) : (<p className=' text-green-500'>{items.status}</p>)}
-                                </div>
-                                <div className='p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[13%]'>COD</div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+      {order && order.length > 0 && (
+        <div className=" md:p-4 p-1 space-y-8">
+          <h1 className=" text-4xl text-zinc-500 font-semibold">
+            Your Order History
+          </h1>
+          <div className=" w-full bg-zinc-800">
+            <div className=" text-zinc-200 text-center border-[1px] border-zinc-700 flex text-sm md:text-xl  ">
+              <div className=" p-2 border-[1px] flex justify-center items-center  border-zinc-600 w-[5%]">
+                Sr.
+              </div>
+              <div className=" p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[25%]">
+                Books
+              </div>
+              <div className=" p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[30%]">
+                Description
+              </div>
+              <div className=" p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[10%]">
+                Price
+              </div>
+              <div className=" p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[18%] ">
+                Status
+              </div>
+              <div className=" p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[13%]">
+                Mode
+              </div>
             </div>
-
-            }
-        </>
-    )
+            <div className=" text-sm w-full ">
+              {order &&
+                order.map((items, i) => (
+                  <div
+                    key={i}
+                    className="  font-semibold text-zinc-300 text-center border-[1px] text-[12px] md:text-sm  h-fit border-zinc-600 w-full flex"
+                  >
+                    <div className="p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[5%]">
+                      {i + 1}
+                    </div>
+                    <div className="p-2 border-[1px] flex justify-center items-center border-zinc-600 hover:underline hover:text-blue-500 transition-all duration-300 w-[25%]">
+                      <Link to={`/view-book-details/${items.book._id}`}>
+                        {items.book.title}
+                      </Link>
+                    </div>
+                    <div className="p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[30%]">
+                      {items.book.desc.slice(0, 40)}...
+                    </div>
+                    <div className="p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[10%]">
+                      &#8377; {items.book.price}
+                    </div>
+                    <div className="p-2 border-[1px] flex justify-center items-center border-zinc-600 text-green-500 w-[18%]">
+                      {items.status === "Ordered Placed" ? (
+                        <p className=" text-yellow-500">{items.status}</p>
+                      ) : items.status === "Cancelled" ? (
+                        <p className="text-red-500">{items.status}</p>
+                      ) : items.status === "Delivered" ? (
+                        <p className=" text-blue-600">{items.status}</p>
+                      ) : (
+                        <p className=" text-green-500">{items.status}</p>
+                      )}
+                    </div>
+                    <div className="p-2 border-[1px] flex justify-center items-center border-zinc-600 w-[13%]">
+                      COD
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default UserOrderHistory
+export default UserOrderHistory;
