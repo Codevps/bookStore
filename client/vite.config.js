56e44a5bfc82e6061b206696386bca79adcbc4a0
Pratham Sawant <codevps07@gmail.com>
2024-09-04 19:26:45 +0530

    done connecting frontend to backend for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: true,
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://book-store-server-pi.vercel.app/",
     },
   },
 });
